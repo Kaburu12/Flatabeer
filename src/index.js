ce65952063b1dc2790be08fbd52f28c6bb5a3181
Kaburu12 <michael.kaburur@student.moringaschool.com>
2022-06-21 23:50:07 +0300

    receive data and display first beer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 // Code here
 document.addEventListener('DOMContentLoaded', () => {
 
-	fetch("http://localhost:3000/beers")
-    .then((res)=>res.json())
+	const beerName = document.querySelector("#beer-name")
+	const beerImage = document.querySelector("#beer-image")
+	const beerDescription = document.querySelector("#beer-description")
+	const reviewList = document.getElementById("review-list")
 
+	fetch("http://localhost:3000/beers")
+		.then((res) => res.json())
+		.then((beersData) => {
+			beerName.textContent = beersData[ 0 ].name
+			beerImage.src = beersData[ 0 ].image_url
+			beerDescription.textContent = beersData[ 0 ].description
+		})
 
 
 
