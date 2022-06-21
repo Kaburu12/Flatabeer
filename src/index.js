34615c5b99964514fac40c69a7b9f357e7ab053d
Samson <samblint@gmail.com>
2022-06-21 23:00:55 +0300

    receive data response

--- src/index.js
+++ src/index.js
@@ -3,4 +3,18 @@
 function beerNavList() {
 	fetch("http://localhost:3000/beers")
 		.then((res) => res.json())
-}   
+       .then((beersData) => {
+			let data1 = "";
+			beersData.map((values) => {
+				data1 += ` <ul id="beer-list">
+				<li></li></ul>
+				<div class="beer-details">
+			
+				<p>
+				  <em id="beer-description"></em>
+				</p>
+				<h3></h3> </div>
+				`
+			})
+		})
+    }
