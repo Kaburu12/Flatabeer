fba462b7cf83c5ca7dd291abb08d3133a191527b
Kaburu12 <michael.kaburur@student.moringaschool.com>
2022-06-22 00:06:32 +0300

    add new reviews on page

--- src/index.js
+++ src/index.js
@@ -28,11 +28,11 @@ document.addEventListener('DOMContentLoaded', () => {
 const reviewForm = document.getElementById('review-form')
 reviewForm.addEventListener('submit',(e)=>{
 	e.preventDefault()
-	// let newReview = document.getElementById("review").value
-	// let reviewUpdate = document.createElement('li')
-	// reviewUpdate.innerText = newReview
-	// reviewList.appendChild(reviewUpdate)
-	// reviewForm.reset()
+	let newReview = document.getElementById("review").value
+	let reviewUpdate = document.createElement('li')
+	reviewUpdate.innerText = newReview
+	reviewList.appendChild(reviewUpdate)
+	reviewForm.reset()
 	// reviewUpdate.addEventListener('click',(e)=>{
 	// 	e.preventDefault()
 	// 	reviewUpdate.remove()
