ef57d54029692f8aa5f78a772b9a5e7ecdd2c456
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-06-22 00:16:36 +0300

    fetch data and render beer

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchData()
       })
 
-      function fetchData() {
-        fetch("http://localhost:3000/beers")
-            .then(response => response.json())
+function fetchData() {
+   fetch("http://localhost:3000/beers")
+        .then(response => response.json())
             .then(data => {
                 return renderBeers(data);
             })
     }     
+
+
+    fetchData();
+    function renderBeers(beers) {
+        beers.forEach(beer => {   
+        
+        }
